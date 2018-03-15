51bd9a869f1d9acaf8f505a5071a7a61039046be
harryxue1991 <gdff64538645@163.com>
2018-03-15 16:29:02 +0800

    添加stage-0

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
             <div>hello world</div>
+            <div>{{msg}}</div>
     </div>
 </template>
 <script>
     export default {
         data () {
-            
+            return {
+                msg:'111'
+            }
         }
     }
 </script>
--- demo3/src/main.js
+++ demo3/src/main.js
@@ -3,7 +3,6 @@ import 'styles/main.scss';
 import Vue from 'vue'
 import App from './App.vue'
 
-
 new Vue({
         el: '#app',
         render: h => h(App)
