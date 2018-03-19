68d353fa6a1d90c1e17891ba4e5eebb2d2a03151
harryxue1991 <gdff64538645@163.com>
2018-03-19 14:02:04 +0800

    aa

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -1,26 +1,15 @@
 <template>
     <div>
-<<<<<<< HEAD
-            <div>hello world</div>
-            <div>{{msg}}</div>
-=======
             <div>hello world!</div><br>
             <div v-html='msg'></div>
->>>>>>> 1af898ba6ba2f5310aab8815fabd6186c6f3bb7f
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-<<<<<<< HEAD
-                msg:'111'
-            }
-=======
                 msg:'Harry Xue'
             }
-            
->>>>>>> 1af898ba6ba2f5310aab8815fabd6186c6f3bb7f
         }
     }
 </script>
